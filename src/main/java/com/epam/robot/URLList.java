71199828185cbfba5687e3d072bdbd0c21865fc4
Adrian <adriandrabik@gmail.com>
2016-03-18 09:31:14 +0100

    Second test for URLList class. Obvious implementetion - we added a List<URL>.

--- src/main/java/com/epam/robot/URLList.java
+++ src/main/java/com/epam/robot/URLList.java
@@ -1,17 +1,25 @@
 package com.epam.robot;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class URLList {
-    public void add(URL example) {
+    List<URL> urls;
 
+    public URLList() {
+        urls = new ArrayList<>();
+    }
+
+    public void add(URL example) {
+        urls.add(example);
     }
 
     public int size() {
-        return 1;
+        return urls.size();
     }
 
     public URL get(int i) {
-        return null;
+        return urls.get(i);
     }
 }
