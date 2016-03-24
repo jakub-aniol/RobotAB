d36b99964470bbb3a60fccf16ee789b5550bcc65
Adrian <adriandrabik@gmail.com>
2016-03-24 14:53:49 +0100

    URL now is downloaded to String and then converted to FileInputStream.

--- src/main/java/com/epam/robot/url/Downloader.java
+++ src/main/java/com/epam/robot/url/Downloader.java
@@ -3,8 +3,8 @@ package com.epam.robot.url;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
@@ -16,6 +16,7 @@ import java.net.URL;
 public class Downloader {
     private static final Logger log = LogManager.getLogger();
     private URL url;
+    private static int counter;
 
     /**
      * Creates an object with information about URL address of future connection.
@@ -27,16 +28,32 @@ public class Downloader {
 
     /**
      * This method starts a connection and returns a stream with content found at the url address.
+     * The connection have 30 sec. timeout for downloading resources. It download a page content
+     * and the close the connection.
      * @return <code>InputStream</code> object with content of the URL address.
      */
     public InputStream getStream() {
         InputStream in=null;
         try {
-            in = url.openStream();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setInstanceFollowRedirects(false);
+            connection.setConnectTimeout(30*1000);
+            connection.setRequestMethod("GET");
+            connection.connect();
+            in = connection.getInputStream();
+            StringBuilder out = new StringBuilder();
+            int c;
+            while ((c=in.read())!=-1) {
+                out.append((char)c);
+            }
+            FileOutputStream output = new FileOutputStream(new File("download"+counter));
+            InputStream input = new FileInputStream(new File("download"+counter));
+            counter++;
+            return input;
         } catch (IOException e) {
             log.error(e.toString());
         }
-        return in;
+        return null;
     }
 
     /**
